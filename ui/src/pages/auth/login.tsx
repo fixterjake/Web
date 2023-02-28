f1d5328397d85e4f21a888dc1796c1c31edb9707
Jacob Boyles <fixterjake@gmail.com>
2023-02-28 11:44:08 -0500

    another method of redirecting to auth

--- ui/src/pages/auth/login.tsx
+++ ui/src/pages/auth/login.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function LoginPage() {
     useEffect(() => {
-        window.location.assign(`${process.env.NEXT_PUBLIC_API_URL}/auth/login/redirect`);
+        window.location.replace(`${process.env.NEXT_PUBLIC_API_URL}/auth/login/redirect`);
     });
 
     return (
